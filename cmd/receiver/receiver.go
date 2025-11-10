566e3eb6fa4f1cb1c7517a13be96d7c8bba79086
Katie Kloss <kloss.katherine@gmail.com>
2025-11-10 18:01:59 -0500

    feat(receiver): new log shipper utility.

---
+++ cmd/receiver/receiver.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+
+	nomad "github.com/hashicorp/nomad/api"
+	"github.com/simonfrey/jsonl"
+)
+
+func main() {
+	var headers = map[string][]string{
+		"X-Nomad-Token": {os.Getenv("NOMAD_TOKEN")},
+	}
+
+	nomad_client, err := nomad.NewClient(&nomad.Config{
+		Address: os.Getenv("NOMAD_UNIX_ADDR"),
+		Headers: headers,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	allocs, _, err := nomad_client.Allocations().List(&nomad.QueryOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	wg := &sync.WaitGroup{}
+
+	for _, alloc := range allocs {
+		job, _, err := nomad_client.Jobs().Info(alloc.JobID, &nomad.QueryOptions{})
+		if err != nil {
+			log.Printf("No access to %s job: %s", *job.ID, err.Error())
+			continue
+		} else if *job.ID == "receiver" {
+			continue
+		}
+
+		wg.Add(1)
+		go logAlloc(nomad_client, alloc, wg)
+	}
+
+	wg.Wait()
+}
+
+func logAlloc(nomad_client *nomad.Client, stub *nomad.AllocationListStub, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+
+	cancel := make(chan struct{})
+	alloc, _, err := nomad_client.Allocations().Info(stub.ID, &nomad.QueryOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for task_name := range alloc.TaskStates {
+		waitGroup.Add(2)
+		go logAllocTask(nomad_client, alloc, task_name, "stdout", &cancel, waitGroup)
+		go logAllocTask(nomad_client, alloc, task_name, "stderr", &cancel, waitGroup)
+	}
+}
+
+type NomadMeta struct {
+	Alloc string `json:"alloc"`
+	Job   string `json:"job"`
+	Group string `json:"group"`
+	Task  string `json:"task"`
+	Node  string `json:"node"`
+}
+
+type OurMeta struct {
+	Type string `json:"type"`
+}
+
+type LogLine struct {
+	Msg   string     `json:"_msg"`
+	Time  string     `json:"_time"`
+	Nomad *NomadMeta `json:"nomad"`
+	Agent *OurMeta   `json:"agent"`
+}
+
+func logAllocTask(nomad_client *nomad.Client, alloc *nomad.Allocation, taskName string, logName string, cancel *chan struct{}, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+
+	http := &http.Client{}
+
+	taskLogs, _ := nomad_client.AllocFS().Logs(alloc, true, taskName, logName, "end", 0, *cancel, &nomad.QueryOptions{})
+
+	for frame := range taskLogs {
+		line := &LogLine{
+			Msg:  string(frame.Data),
+			Time: "0",
+			Nomad: &NomadMeta{
+				Alloc: alloc.ID,
+				Job:   alloc.JobID,
+				Group: alloc.TaskGroup,
+				Task:  taskName,
+				Node:  alloc.NodeID,
+			},
+			Agent: &OurMeta{
+				Type: "receiver",
+			},
+		}
+
+		buf := &bytes.Buffer{}
+		jsonl.NewWriter(buf).Write(line)
+
+		url := fmt.Sprintf("http://%s/insert/jsonline?_stream_fields=nomad.alloc,nomad.job", os.Getenv("VICTORIALOGS_ADDR"))
+		resp, err := http.Post(url, "application/stream+json", bytes.NewReader(buf.Bytes()))
+
+		if err != nil {
+			panic(err)
+		}
+
+		if resp.StatusCode != 200 {
+			log.Println(resp)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,19 +1,23 @@
 module sentry
 
-go 1.24.2
+go 1.24.7
 
-require github.com/hashicorp/nomad/api v0.0.0-20251104171905-3f42610f2ab1
+toolchain go1.24.10
+
+require github.com/hashicorp/nomad/api v0.0.0-20230103221135-ce00d683f9be
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/cronexpr v1.1.3 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.2-0.20210821155943-2d9075ca8770 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/shoenig/test v1.12.1 // indirect
+	github.com/simonfrey/jsonl v0.0.0-20240904112901-935399b9a740 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 )
